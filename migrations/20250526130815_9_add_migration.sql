1164a8b5b932554f2747b553615005560cb5f267
Alex Stepanenco <stepnenca@gmail.com>
2025-05-26 16:48:32 +0300

    add goose migrations and add tables

--- migrations/20250526130815_9_add_migration.sql
+++ migrations/20250526130815_9_add_migration.sql
@@ -1,7 +1,14 @@
-
+-- +goose Up
+-- +goose StatementBegin
 CREATE TABLE employee (
     id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     name TEXT NOT NULL,
     created_at TIMESTAMPTZ DEFAULT NOW() NOT NULL,
     updated_at TIMESTAMPTZ DEFAULT NOW() NOT NULL
-)
+);
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+DROP TABLE employee;
+-- +goose StatementEnd
